76576e2aafea8d16ed5abc83131f63cab4ea4ed3
Michal Freriks <michal.freriks@gmail.com>
2025-01-09 21:43:19 +0100

    fix

--- lib/viewmodel/timer_running_viewmodel.dart
+++ lib/viewmodel/timer_running_viewmodel.dart
@@ -21,6 +21,12 @@ class TimerRunningState {
     required this.isFinished,
   });
 
+  TimerRunningState.initial()
+      : currentRound = 1,
+        remainingTime = 0,
+        isBreak = false,
+        isFinished = false;
+
   TimerRunningState copyWith({
     int? currentRound,
     int? remainingTime,
@@ -40,34 +46,17 @@ class TimerRunningViewModel extends StateNotifier<TimerRunningState> {
   Timer? _timer;
   final Ref ref;
 
-  TimerRunningViewModel(this.ref)
-      : super(TimerRunningState(
-    currentRound: 0,
-    remainingTime: 0,
-    isBreak: false,
-    isFinished: false,
-  )) {
-    _startNextPhase();
-  }
+  TimerRunningViewModel(this.ref) : super(TimerRunningState.initial());
 
-  void _startNextPhase() {
+  void start() {
     final settings = ref.read(timerViewModelProvider);
-    if (state.isBreak) {
-      _initializePhase(settings.breakDuration.inSeconds, false);
-    } else {
-      if (state.currentRound < settings.roundCount) {
-        _initializePhase(settings.roundDuration.inSeconds, true);
-        state = state.copyWith(currentRound: state.currentRound + 1);
-      } else {
-        _finishTimer();
-      }
-    }
+    _initializePhase(settings.roundDuration.inSeconds, isBreak: false);
   }
 
-  void _initializePhase(int duration, bool isBreakPhase) {
+  void _initializePhase(int duration, {required bool isBreak}) {
     state = state.copyWith(
       remainingTime: duration,
-      isBreak: isBreakPhase,
+      isBreak: isBreak,
     );
     _startCountdown();
   }
@@ -79,9 +68,24 @@ class TimerRunningViewModel extends StateNotifier<TimerRunningState> {
         state = state.copyWith(remainingTime: state.remainingTime - 1);
       } else {
         timer.cancel();
-        _startNextPhase();
+        _transitionToNextPhase();
       }
     });
+  }
+
+  void _transitionToNextPhase() {
+    final settings = ref.read(timerViewModelProvider);
+
+    if (state.isBreak) {
+      if (state.currentRound < settings.roundCount) {
+        state = state.copyWith(currentRound: state.currentRound + 1);
+        _initializePhase(settings.roundDuration.inSeconds, isBreak: false);
+      } else {
+        _finishTimer();
+      }
+    } else {
+      _initializePhase(settings.breakDuration.inSeconds, isBreak: true);
+    }
   }
 
   void _finishTimer() {

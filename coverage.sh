d64671de7c492b23f98915ab967dcd1d1a0503b9
Michal Freriks <michal.freriks@gmail.com>
2025-01-09 19:34:04 +0100

    hack

--- coverage.sh
+++ coverage.sh
@@ -5,5 +5,6 @@ set -e
 flutter test --coverage
 #lcov --remove coverage/lcov.info 'lib/*.g.dart' -o coverage/new_lcov.info
 lcov --remove coverage/lcov.info  -o coverage/new_lcov.info
-genhtml coverage/new_lcov.info -o coverage/html | grep "lines\.*:" | dart run coverage_check.dart 100.0
+genhtml coverage/new_lcov.info -o coverage/html --config-file coverage.config
 open coverage/html/index.html
+genhtml coverage/new_lcov.info -o coverage/html | grep "lines\.*:" | dart run coverage_check.dart 100.0
